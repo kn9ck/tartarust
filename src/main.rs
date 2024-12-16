62d9bb2759eb627c915db7caf33627613cc737b7
kn9ck <oknackk@gmail.com>
2024-12-15 20:47:20 -0700

    minimal rust binary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std] // dont link rust standard lib
+#![no_main] // disable main entry point
+
+use core::panic::PanicInfo;
+
+//called on panic
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+//new entry point
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop{}
 }
