1ac8fbf68271fcd350e1dfba5b3053564d1e729f
kn9ck <oknackk@gmail.com>
2024-12-16 16:11:02 -0700

    safe vga buffer read/write

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,19 @@
 
 mod vga_buffer;
 
-use core::panic::PanicInfo;
-
-static HELLO: &[u8] = b"Hello World!";
 
 //new entry point
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
-
+    println!("Hello World{}", "!");
+    
     loop {}
 }
 
+use core::panic::PanicInfo;
 //called on panic
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
