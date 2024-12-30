34253cf576487ce481656fb948f97eabc4222af7
kn9ck <oknackk@gmail.com>
2024-12-30 09:25:05 -0700

    dynamic allocation

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@
 
 extern crate alloc;
 
-use alloc::boxed::Box;
+use alloc::{boxed::Box, rc::Rc, vec, vec::Vec};
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 use tartarust::println;
@@ -14,6 +14,7 @@ use tartarust::println;
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use tartarust::allocator;
     use tartarust::memory;
     use tartarust::memory::BootInfoFrameAllocator;
     use x86_64::{structures::paging::Page, VirtAddr};
@@ -25,7 +26,26 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
     let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
-    let x = Box::new(41);
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap init failed");
+
+    //test dynamic allocation
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
+    }
+    println!("vec at {:p}", vec.as_slice());
+
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+    println!(
+        "current reference count is {}",
+        Rc::strong_count(&cloned_reference)
+    );
+    core::mem::drop(reference_counted);
+    println!(
+        "reference count is {} now",
+        Rc::strong_count(&cloned_reference)
+    );
 
     #[cfg(test)]
     test_main();
